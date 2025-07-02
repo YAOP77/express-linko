17af412c9cf9535e4bd919ee2d3e793ccb911d7a
YAOP77 <yaop77@gmail.com>
2025-07-02 09:24:12 +0000

    fichier uplo

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const { searchUsers, getOnlineUsers, getUserById, updateUserProfile, uploadAvatar, blockUser, unblockUser, isBlocked, getAllUsers, setAdmin, createReport, getAllReports, deleteUser, banUser, unbanUser } = require("../controllers/user.controller");
+const {
+  searchUsers,
+  getOnlineUsers,
+  getUserById,
+  updateUserProfile,
+  uploadAvatar,
+  blockUser,
+  unblockUser,
+  isBlocked,
+  getAllUsers,
+  setAdmin,
+  createReport,
+  getAllReports,
+  deleteUser,
+  banUser,
+  unbanUser,
+  upload
+} = require("../controllers/user.controller");
 const verifyToken = require("../middelware/verifyToken");
 
 router.get("/search", verifyToken, searchUsers);
@@ -8,7 +25,8 @@ router.get("/online", getOnlineUsers);
 router.get("/all", verifyToken, getAllUsers);
 router.get("/:id", verifyToken, getUserById);
 router.put("/:id", verifyToken, updateUserProfile);
-router.post("/:id/avatar", verifyToken, uploadAvatar);
+// router.post("/:id/avatar", verifyToken, uploadAvatar);
+router.post("/:id/avatar", verifyToken, upload.single("avatar"), uploadAvatar);
 router.post("/:id/block", verifyToken, blockUser);
 router.post("/:id/unblock", verifyToken, unblockUser);
 router.get("/:id/is-blocked", verifyToken, isBlocked);
