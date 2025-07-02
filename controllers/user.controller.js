90b9132fc9def16358595c5e8a5d6134b535ac53
YAOP77 <yaop77@gmail.com>
2025-07-02 10:21:13 +0000

    fichier uplo

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,11 +2,17 @@ const User = require("../models/User");
 const multer = require('multer');
 const path = require('path');
 const Report = require('../models/Report');
+const fs = require('fs');
 
 // Config multer pour les avatars
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../uploads'));
+    const uploadPath = path.join(__dirname, '../uploads');
+    // Vérifie/crée le dossier à chaque upload
+    if (!fs.existsSync(uploadPath)) {
+      fs.mkdirSync(uploadPath, { recursive: true });
+    }
+    cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
